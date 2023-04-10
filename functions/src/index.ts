aed3308069097cfea31adec2874329233ea41f72
Tsubasa Nagata <25589480+ototonari@users.noreply.github.com>
2023-04-11 00:26:16 +0900

    fix region

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
 import {startUp} from "./models/openAI";
-import {requestHandler} from "./io/function";
+import {externalHandler, requestHandler} from "./io/function";
 import {requestConnector} from "./io/requestConnector";
 import {simpleQuestion, simpleQuestionOnSlack} from "./usecase/simpleQuestion";
 import {helloWorld} from "./usecase/test";
@@ -13,7 +13,7 @@ admin.initializeApp();
 startUp(functions.config().open_ai.api_key);
 
 const defaultConnector = requestConnector(requestHandler);
-const slackConnector = requestConnectorOnSlack(requestHandler);
+const slackConnector = requestConnectorOnSlack(externalHandler);
 
 export const askChatGPT = defaultConnector(simpleQuestion);
 
