1ce4a9aa517faee28a981b643b752f0d247d270a
Tsubasa Nagata <25589480+ototonari@users.noreply.github.com>
2023-04-11 01:20:29 +0900

    fix base handler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import {externalHandler, requestHandler} from "./io/function";
+import {requestHandler} from "./io/function";
 import {requestConnector} from "./io/requestConnector";
 import {simpleQuestion, simpleQuestionOnSlack} from "./usecase/simpleQuestion";
 import {helloWorld} from "./usecase/test";
@@ -8,10 +8,8 @@ import {startup} from "./startup";
 startup();
 
 const defaultConnector = requestConnector(requestHandler);
-const slackConnector = requestConnectorOnSlack(externalHandler);
-
 export const askChatGPT = defaultConnector(simpleQuestion);
-
-export const askQuestion = slackConnector(simpleQuestionOnSlack);
-
 export const test = defaultConnector(helloWorld);
+
+const slackConnector = requestConnectorOnSlack(requestHandler);
+export const askQuestion = slackConnector(simpleQuestionOnSlack);
--- functions/src/io/function.ts
+++ functions/src/io/function.ts
@@ -2,15 +2,11 @@ import * as functions from "firebase-functions";
 
 const runtime = functions.runWith({timeoutSeconds: 540, memory: "128MB"});
 
-const usRegion = runtime.region("us-central1");
-
 const tokyoRegion = runtime.region("asia-northeast1");
 
 // リージョンやインスタンスの設定はこちらで管理する
 export const requestHandler = tokyoRegion
   .https
   .onRequest;
-
-export const externalHandler = usRegion.https.onRequest;
 
 export const logger = functions.logger;
