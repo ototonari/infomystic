49f3a31949ce7669d880c6c1e5b702826366410c
Tsubasa Nagata <25589480+ototonari@users.noreply.github.com>
2023-04-23 12:47:40 +0900

    update event handler

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
-import {requestHandler} from "./io/function";
-import {requestConnector} from "./io/requestConnector";
-import {simpleQuestion, simpleQuestionOnSlack} from "./usecase/simpleQuestion";
-import {helloWorld} from "./usecase/test";
-import {requestConnectorOnSlack} from "./io/requestConnectorOnSlack";
-import {startup} from "./startup";
+import { requestHandler } from "./io/function";
+import { requestConnector } from "./io/requestConnector";
+import {
+  simpleQuestion,
+  simpleQuestionOnSlack,
+} from "./usecase/simpleQuestion";
+import { helloWorld } from "./usecase/test";
+import { requestConnectorOnSlack } from "./io/requestConnectorOnSlack";
+import { startup } from "./startup";
 import { conversationChatOnSlack } from "./usecase/conversationChat";
 
 startup();
--- functions/src/startup.ts
+++ functions/src/startup.ts
@@ -1,13 +1,12 @@
 import * as admin from "firebase-admin";
 
-import {startUp} from "./models/openAI";
-import {Config} from "./io/firebaseConfig";
-import {initSlackWebClient} from "./models/slack";
+import { startUp } from "./models/openAI";
+import { Config } from "./io/firebaseConfig";
+import { initSlackWebClient } from "./models/slack";
 
 export const startup = async (callback?: () => Promise<void>) => {
   admin.initializeApp();
   initSlackWebClient(Config.SlackBotToken);
   await startUp(Config.OpenAIKey);
-  // await startUpByLC(Config.OpenAIKey);
   if (callback) return callback();
 };
